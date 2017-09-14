a5c16bf7801250cfac38a99233fd13269280c24d
Dagorik <dagorik@gmail.com>
2017-09-13 20:44:47 -0500

    runtime permission

---
+++ runtimepermision/src/main/java/batch16/android/devf/com/runtimepermision/Constants.java
@@ -0,0 +1,15 @@
+package batch16.android.devf.com.runtimepermision;
+
+/**
+ * Created by Dagorik on 13/09/17.
+ */
+
+public class Constants {
+    public static final String INTENT_KEY_USERNAME = "USER_NAME";
+    public static final int REQUEST_CODE_FOTO = 102;
+    public static final int REQUEST_PERMISSION_CAMERA = 300;
+
+    public static final int REQUEST_CODE_SECOND_ACTIVITY = 101;
+    public static final String INTENT_KEY_SALUDO="Saludosssss";
+
+}
